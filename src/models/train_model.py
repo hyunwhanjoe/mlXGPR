b78c42d4b708735f5512254d0019db90562d3e8e
hyunwhanjoe <hyunwhanjoe@gmail.com>
2023-07-14 17:02:03 +0000

    create train_model.py

---
+++ src/models/train_model.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+import time
+import csv
+
+import xgboost as xgb
+
+import sys
+sys.path.append('../utility')
+from utility import read_data
+
+def log(model_name,t1):
+    with open('running_time.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csvwriter.writerow(['model_name','time_s', 'time_m'])
+        csvwriter.writerow([model_name, round(t1,1), round(t1/60,2)])
+
+data_path = Path('../../data/processed')
+y_type = 'int64'
+X_path = data_path/'Synset-2_X.npy'
+y_path = data_path/'Synset-2_y.npy'
+
+X_train, y_train = read_data(X_path, y_path, y_type)
+clf = xgb.XGBClassifier(n_jobs=35,
+                        tree_method='hist',
+                        max_depth=4,
+                        n_estimators=19)
+
+t0 = time.time()
+clf.fit(X_train, y_train)
+t1 = time.time() - t0
+
+model_name = 'model.json'
+log(model_name, t1)
+
+model_path = Path('../../models')
+clf.save_model(model_path/model_name)
+print(model_path/model_name)
