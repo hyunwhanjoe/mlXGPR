a4eb4f791bddef66708faf337c5330010ee46770
hyunwhanjoe <hyunwhanjoe@gmail.com>
2023-07-15 18:46:05 +0000

    train_model.py - write n_jobs to file

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -8,20 +8,21 @@ import sys
 sys.path.append('../utility')
 from utility import read_data
 
-def log(model_name,t1):
+def log(model_name,t1, n_jobs):
     with open('running_time.csv', 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        csvwriter.writerow(['model_name','time_s', 'time_m'])
-        csvwriter.writerow([model_name, round(t1,1), round(t1/60,2)])
+        csvwriter.writerow(['model_name', 'n_jobs', 'time_s', 'time_m'])
+        csvwriter.writerow([model_name, n_jobs, round(t1,1), round(t1/60,2)])
 
 data_path = Path('../../data/processed')
 y_type = 'int64'
 X_path = data_path/'Synset-2_X.npy'
 y_path = data_path/'Synset-2_y.npy'
-
 X_train, y_train = read_data(X_path, y_path, y_type)
-clf = xgb.XGBClassifier(n_jobs=35,
+
+n_jobs=10
+clf = xgb.XGBClassifier(n_jobs=n_jobs,
                         tree_method='hist',
                         max_depth=4,
                         n_estimators=19)
@@ -31,7 +32,7 @@ clf.fit(X_train, y_train)
 t1 = time.time() - t0
 
 model_name = 'model.json'
-log(model_name, t1)
+log(model_name, t1, n_jobs)
 
 model_path = Path('../../models')
 clf.save_model(model_path/model_name)
